a1c0423045fbfe9955cb0612b77c1c7766ebbbfd
Boyle, William <boywi03@ca.com>
2016-04-04 12:11:26 -0400

    Updates include:
    - Sample project to demonstrate the bootRepackage issue found with grails 3.1.3, war plugin, pathingJar enabled that picks up the jar file instead of the war.

---
+++ grails313/grails-app/assets/javascripts/grails313/grails313.js
@@ -0,0 +1,9 @@
+//= wrapped
+//= require /angular/angular
+//= require /grails313/core/grails313.core
+//= require /grails313/index/grails313.index
+
+angular.module("grails313", [
+    "grails313.core",
+    "grails313.index"
+]);
